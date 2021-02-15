7d8e841557cbc085d60ccce9c99e3815ec18b7f0
Jakub Rożek <jakub@stoplight.io>
2021-02-15 14:21:54 +0200

    chore: bump json-ref-readers

--- src/resolvers/http-and-file.ts
+++ src/resolvers/http-and-file.ts
@@ -1,9 +1,7 @@
-import { createResolveHttp } from '@stoplight/json-ref-readers';
+import { createResolveHttp, resolveFile } from '@stoplight/json-ref-readers';
 import { Resolver } from '@stoplight/json-ref-resolver';
 import { DEFAULT_REQUEST_OPTIONS } from '../request';
 import { Agent } from 'http';
-import { readFile } from 'fs';
-import * as URI from 'urijs';
 
 export interface IHttpAndFileResolverOptions {
   agent?: Agent;
@@ -15,24 +13,11 @@ export const httpAndFileResolver = createHttpAndFileResolver();
 export function createHttpAndFileResolver(opts?: IHttpAndFileResolverOptions): Resolver {
   const resolveHttp = createResolveHttp({ ...DEFAULT_REQUEST_OPTIONS, ...opts });
 
-  // Note: this should be better fixed at @stoplight/json-ref-readers level
-  const patchedResolved = (ref: URI): Promise<unknown> => {
-    return new Promise((resolve, reject) => {
-      const path = ref.valueOf();
-      readFile(path, 'utf8', (err, data) => {
-        if (err !== null) {
-          reject(err);
-        }
-        resolve(data);
-      });
-    });
-  };
-
   return new Resolver({
     resolvers: {
       https: { resolve: resolveHttp },
       http: { resolve: resolveHttp },
-      file: { resolve: patchedResolved },
+      file: { resolve: resolveFile },
     },
   });
 }
